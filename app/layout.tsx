0b622a51c7ca29d6e89497e57b91a85af70afde3
Pascal Bourque <pascal@studyo.co>
2023-12-11 13:52:12 -0500

    Bug repro

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+import { GoogleTagManager } from '@next/third-parties/google'
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
       <body>{children}</body>
+      <GoogleTagManager gtmId='BOGUS_GTM_ID' />
     </html>
   )
 }
